0b4950f14e84efa6e0069799dfddb3fcb736789a
Alexander Cairns <alexandercairns@discoverygarden.ca>
2023-02-14 09:57:57 -0400

    Reverting for testing

--- images/activemq/activemq-alpine/README.md
+++
@@ -1,3 +0,0 @@
-# README.md
-
-In activemq.xml, the Openwire and Stomp listening IP needs to change from 127.0.0.1 to 0.0.0.0
--- images/solr/solr-alpine/solr.in.sh
+++
@@ -1,3 +0,0 @@
-# https://solr.apache.org/guide/8_1/basic-authentication-plugin.html#enable-basic-authentication
-SOLR_AUTH_TYPE=basic
-SOLR_AUTHENTICATION_OPTS=-Dbasicauth=drupal:drupal
