9649fef46e01d1ca758f5271f17041519377c771
Alexander Cairns <alexandercairns@discoverygarden.ca>
2025-11-13 15:21:29 -0400

    add jmx opt

--- solr.in.sh
+++ solr.in.sh
@@ -1,4 +1,4 @@
 # https://solr.apache.org/guide/8_1/basic-authentication-plugin.html#enable-basic-authentication
 SOLR_AUTH_TYPE=basic
 SOLR_AUTHENTICATION_OPTS=-Dbasicauth=drupal:drupal
-SOLR_OPTS="$SOLR_OPTS -Dsolr.hocr.plugin.path=${SOLR_HOCR_PLUGIN_PATH}"
+SOLR_OPTS="$SOLR_OPTS -javaagent:/jmx/jmx_prometheus_javaagent.jar=3001:/jmx/jmx.yml -Dsolr.hocr.plugin.path=${SOLR_HOCR_PLUGIN_PATH}"
